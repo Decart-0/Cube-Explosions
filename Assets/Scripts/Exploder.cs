e930ae8358974a2c4789f6970339e7cf1bb1c823
Decart-0 <165915042+Decart-0@users.noreply.github.com>
2024-11-11 17:17:55 +0300

    Add files via upload

---
+++ Assets/Scripts/Exploder.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exploder : MonoBehaviour
+{
+    [SerializeField] private float _explosionForce;
+    [SerializeField] private float _explosionRadius;
+
+    public void ExplodeCube(Vector3 position, List<Rigidbody> cubes)
+    {
+        foreach (Rigidbody explodableObject in cubes)
+        {
+            explodableObject.AddExplosionForce(_explosionForce, position, _explosionRadius);
+        }
+    }
+}
